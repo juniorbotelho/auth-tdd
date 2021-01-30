d99806ac68dd71a24a1d08524237f3a847d3c41d
juniorbotelho <junowerther@gmail.com>
2021-01-30 18:22:58 -0300

    feat: configure the project to support environment variables

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+require('dotenv')
+  .config({
+    path: process.env.NODE_ENV === 'test' ?
+      '.env.test' : '.env'
+  })
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const Router = require('./app/routes')
--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,10 +1,17 @@
+require('dotenv')
+  .config({
+    path: process.env.NODE_ENV === 'test' ?
+      '.env.test' : '.env'
+  })
+
 module.exports = {
-  host: 'localhost',
-  port: 3306,
-  database: 'authtdd',
-  username: 'root',
-  password: 'KEqNTAloy',
-  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  dialect: process.env.DB_DIALECT || 'mysql',
+  storage: './tests/index.sqlite',
   logging: false,
   define: {
     timestamps: true,
