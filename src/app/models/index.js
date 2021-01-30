a722f7d79335a2ef5dff5dddea5836391782dbc6
juniorbotelho <junowerther@gmail.com>
2021-01-30 17:41:11 -0300

    refact: refactor the 'models/index' file correctly

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -8,7 +8,7 @@ const config = require('../config/database');
 const db = {};
 
 // Sequelize
-sequelize = new Sequelize(
+const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
